488e1d82e3aea5aad61135e15fea8e9d2df99585
leoleoyuuki <rm550373@fiap.com.br>
2023-11-04 10:18:15 -0300

    Init Project

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+
+export default function Home() {
+  return(
+    <>
+      <h1>HOME PAGE</h1>
+
+      
+
+    </>
+  )
+}
---
+++ src/components/Rodape/Rodape.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Rodape() {
+  return (
+    <footer className='rodape'>
+      <div>
+        <p>Desenvolvido por: <span className="font-bold text-blue-400">LeoYuuki</span></p>
+        <p>NEXT.js - React.js - GitHub - TailwindCss</p>
+        </div>
+
+    </footer>
+  )
+}
