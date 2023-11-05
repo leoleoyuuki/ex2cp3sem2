94633e21eec64ac178955e168a50988d3127ce13
leoleoyuuki <rm550373@fiap.com.br>
2023-11-05 12:13:14 -0300

    Adição dos icones e funcoes dos mesmos

--- src/app/consumo/pessoas-view-all/page.jsx
+++ src/app/consumo/pessoas-view-all/page.jsx
@@ -1,42 +1,53 @@
 
 import Link from "next/link";
 import { redirect } from "next/navigation";
+import { AiFillHome, AiFillGithub } from 'react-icons/ai';
 
 export default async function EletroView() {
 
     let items;
 
-    try{
+    try {
         const response = await fetch('http://localhost:3000/dados/get-all');
         items = await response.json();
-        
-    }catch(error){
+
+    } catch (error) {
         console.log(error);
         redirect('/error');
     }
 
-  return (
-    <div>
-        <h1>Página das Pessoas</h1>
-        
-        <div className="card-eletro-view">
-                
+    return (
+        <div>
+            <h1>Página das Pessoas</h1>
+
+            <div className="card-eletro-view">
+
                 <ul>
                     {
-                        items.map((item)=>(
-                            <Link href={`/consumo/pessoas-view-by-id/${item.id}`}>
-                                <li key={item.id}>
-                                    <p>{item.nome}</p>
-                                    <p>{item.postalZip}</p>
-                                    <p>{item.country}</p>
-                                </li>
-                            </Link>
-                        ))
-                    }
-                </ul>
-            
-        </div>
+                        items.map((item) => (
 
-    </div>
-  )
+                            <li key={item.id}>
+                                <Link href={`/consumo/pessoas-view-by-id/${item.id}`}>
+                                    <p className="nome">{item.nome}</p>
+                                </Link>
+                                <p>{item.postalZip}</p>
+                                <p>{item.country}</p>
+                                <div className="icons">
+                                    <Link href="/"><AiFillHome /></Link>
+                                    <Link href="https://github.com/"><AiFillGithub /></Link>
+
+                                </div>
+                            </li>
+
+
+                        ))
+
+                    }
+
+                </ul>
+
+            </div>
+
+        </div>
+    )
 }
--- src/app/consumo/pessoas-view-by-id/[id]/page.jsx
+++ src/app/consumo/pessoas-view-by-id/[id]/page.jsx
@@ -1,34 +1,44 @@
+import { AiFillHome, AiFillGithub } from 'react-icons/ai';
+import {BiArrowBack} from 'react-icons/bi';
 
 import { redirect } from "next/navigation";
+import Link from 'next/link';
 
-export default async function PessoasView({params}) {
+export default async function PessoasView({ params }) {
 
     let item;
     const id = params.id;
 
-    try{    
+    try {
         const response = await fetch(`http://localhost:3000/dados/get-by-id/${id}`);
         item = await response.json();
-        
-    }catch(error){
+
+    } catch (error) {
         console.log(error);
         redirect('/error');
     }
 
-  return (
-    <div>
-        <h1>Página das Pessoas</h1>
-        
-        <div className="card-eletro-view">
-            <ul>
-                <li key={item.id}>
-                    <p>{item.nome}</p>
-                    <p>{item.postalZip}</p>
-                    <p>{item.country}</p>
-                </li>
-            </ul>
-        </div>
+    return (
+        <div>
+            <h1>Página das Pessoas</h1>
 
-    </div>
-  )
+            <div className="card-eletro-view">
+                <ul>
+                    <li key={item.id}>
+                        <p>{item.nome}</p>
+                        <p>{item.postalZip}</p>
+                        <p>{item.country}</p>
+                        <div className="icons">
+                            <Link href="/"><AiFillHome /></Link>
+                            <Link href="https://github.com/"><AiFillGithub /></Link>
+                            <Link href="/consumo/pessoas-view-all"><BiArrowBack /></Link>
+                            
+
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    )
 }
