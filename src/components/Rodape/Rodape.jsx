44086fa658bd26a780e486d8b21550618e7baf55
fatroo <danielsdelfin@gmail.com>
2023-11-06 09:39:03 -0300

    style Footer arrumado

--- src/components/Rodape/Rodape.jsx
+++ src/components/Rodape/Rodape.jsx
@@ -4,7 +4,7 @@ export default function Rodape() {
   return (
     <footer className='rodape'>
       <div>
-        <p>Desenvolvido por: <span className="font-bold text-blue-400">LeoYuuki</span></p>
+        <p>Desenvolvido por: <span className="font-bold text-blue-400">LeoYuuki</span> - <span className='font-bold text-red-500'>Daniel</span> </p>
         <p>NEXT.js - React.js - GitHub - TailwindCss</p>
         </div>
 
